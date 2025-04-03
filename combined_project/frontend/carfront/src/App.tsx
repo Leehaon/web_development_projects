ab3e86f4ff88728b733fe042acf13d935fd73790
Leehaon <global3556@naver.com>
2025-04-03 21:54:58 +0900

    20250403 carlist get 요청

--- combined_project/frontend/carfront/src/App.tsx
+++ combined_project/frontend/carfront/src/App.tsx
@@ -3,6 +3,10 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Carlist from "./components/Carlist";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -15,9 +19,11 @@ function App() {
             </Typography>
           </Toolbar>
         </AppBar>
-
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
     </Container>
-  )
+  );
 }
 
 export default App
