1a8c617f2c953b9ac95e757d16310c244bea74ff
Leehaon <global3556@naver.com>
2025-04-08 21:33:32 +0900

    20250408 1000시간 마무리

--- 10000hoursrule/src/components/ResultBox.jsx
+++ 10000hoursrule/src/components/ResultBox.jsx
@@ -10,11 +10,20 @@ const ResultBox = ({loading, resultVisible, field, time, onModalOpen, onShare}) 
         <Box sx={{display:'flex', justifyContent: 'center', mb: 8}}>
           <img src={LoadingImage} alt="loading" style={{
             animation: 'rotate 1.1s infinite linear',
-            width: 100,
+            width: 100
           }}/>
         </Box>
+      )}
 
-        // 여기서부터 내일하겠습니다.
+      {resultVisible && (
+        <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', lineHeight: 1.5, mb: 8}}>
+          <Typography sx={{fontSize: 20}}>당신은 <span style={{fontSize: 70}}>{field}</span> 전문가가 되기 위해서</Typography>
+          <Typography sx={{fontSize: 20}}>대략 <span style={{fontSize: 70}}>{calculatedDays}</span> 일 이상 훈련하셔야 합니다!!!😎</Typography>
+          <Box sx={{display: 'flex', justifyContent: 'center', mt: 7}}>
+            <Button variant="contained" onClick={onModalOpen} sx={{backgroundColor: '#FCEE21', color: '#5B2386'}}>훈련하러 가기💻</Button>
+            <Button variant="outlined" onClick={onShare} sx={{backgroundColor: '#fff', color: '#5B2386'}}>공유하기</Button>
+          </Box>
+        </Box>
       )}
     </>
   );
